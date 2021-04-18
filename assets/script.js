3ed2e4ae66c6c2939beb70caca8b78ca32dcdffb
EDNARDO DE SOUSA PAZ <eds_paz@hotmail.com>
2021-04-18 17:16:42 -0300

    Atualização Video.
    Section video completa.

---
+++ assets/script.js
@@ -0,0 +1,36 @@
+
+
+var tocar = document.querySelector("#video1");
+
+var playBtn = document.querySelector('.play-btn');
+var pauseBtn = document.querySelector('pause-btn')
+var count = 0;
+
+playBtn.addEventListener("mouseover", opacyti1 )
+playBtn.addEventListener("mouseout", opacyti )
+
+
+function opacyti(){
+    playBtn.style.opacity = 0
+}
+function opacyti1(){
+    playBtn.style.opacity = 1
+}
+
+function play() {
+    if (count == 0) {
+        count = 1;
+        tocar.play()
+        playBtn.innerHTML = "  &nbsp;&#9654;"
+        playBtn.style.color ="white"
+        playBtn.style.fontSize= "20pt"
+
+    } else {
+        count = 0;
+        tocar.pause()
+        playBtn.innerHTML = " ❚ ❚"
+        playBtn.style.color ="white"
+        playBtn.style.fontSize= "19pt"
+
+    }
+}
