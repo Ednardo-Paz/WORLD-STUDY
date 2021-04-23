2a153cf37fb60805759da1e61086ef849c148a1b
EDNARDO DE SOUSA PAZ <eds_paz@hotmail.com>
2021-04-23 00:08:18 -0300

    Atualização contato.
    Atualização contato.

--- assets/script.js
+++ assets/script.js
@@ -6,31 +6,39 @@ var playBtn = document.querySelector('.play-btn');
 var pauseBtn = document.querySelector('pause-btn')
 var count = 0;
 
+
 playBtn.addEventListener("mouseover", opacyti1 )
 playBtn.addEventListener("mouseout", opacyti )
 
 
+
 function opacyti(){
     playBtn.style.opacity = 0
+ 
 }
+
+
 function opacyti1(){
     playBtn.style.opacity = 1
+  
 }
+
 
 function play() {
     if (count == 0) {
         count = 1;
         tocar.play()
         playBtn.innerHTML = " ❚ ❚"
-        playBtn.style.color ="#8e2170"
+        playBtn.style.color ="#f63fed"
         playBtn.style.fontSize= "20pt"
+        
         
 
     } else {
         count = 0;
         tocar.pause()
         playBtn.innerHTML = "   &nbsp;&#9654;"
-        playBtn.style.color ="#8e2170"
+        playBtn.style.color ="#f63fed"
         playBtn.style.fontSize= "19pt"
 
     }
