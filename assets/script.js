52194c00b4dd9cb998ab08e4bfde95ffdc5addf6
EDNARDO DE SOUSA PAZ <eds_paz@hotmail.com>
2021-05-05 13:40:32 -0300

    up
    Evento Mouse corrigido.

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,44 @@
 
 
 var tocar = document.querySelector("#video1");
-
 var playBtn = document.querySelector('.play-btn');
-var pauseBtn = document.querySelector('pause-btn')
 var count = 0;
 
+playBtn.addEventListener(`click`, opacity)
+playBtn.addEventListener(`mouseover`, opacityOn)
+playBtn.addEventListener(`mouseout`, opacityOff)
 
+function opacity() {
+    if (count == 1) {
+        playBtn.style.opacity = 0
+    } else {
+        playBtn.style.opacity = 1
+    }
+} 
+function opacityOn() {
+    if (count == 1) {
+        playBtn.style.opacity = 1
+    }
+}
+function opacityOff() {
+    if (count == 1) {
+        playBtn.style.opacity = 0
+    }
+}
 
 function play() {
     if (count == 0) {
         count = 1;
         tocar.play()
-        playBtn.style.opacity = 0
-
+        playBtn.innerHTML = " ❚ ❚"
+        playBtn.style.color = "#8e2170"
+        playBtn.style.color = "#f63fed"
+        playBtn.style.fontSize = "20pt"
     } else {
         count = 0;
         tocar.pause()
         playBtn.innerHTML = "   &nbsp;&#9654;"
         playBtn.style.color = "#f63fed"
         playBtn.style.fontSize = "19pt"
-        playBtn.style.opacity = 1
-
     }
 }
