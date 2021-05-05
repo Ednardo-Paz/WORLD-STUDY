6390f1f73089394670bcf96f5d1119975fb68614
EDNARDO DE SOUSA PAZ <eds_paz@hotmail.com>
2021-05-05 10:56:08 -0300

    UP
    UP

--- assets/script.js
+++ assets/script.js
@@ -7,39 +7,20 @@ var pauseBtn = document.querySelector('pause-btn')
 var count = 0;
 
 
-playBtn.addEventListener("mouseover", opacyti1 )
-playBtn.addEventListener("mouseout", opacyti )
-
-
-
-function opacyti(){
-    playBtn.style.opacity = 0
- 
-}
-
-
-function opacyti1(){
-    playBtn.style.opacity = 1
-  
-}
-
 
 function play() {
     if (count == 0) {
         count = 1;
         tocar.play()
-        playBtn.innerHTML = " ❚ ❚"
-        playBtn.style.color ="#f63fed"
-        playBtn.style.fontSize= "20pt"
-        
-        
+        playBtn.style.opacity = 0
 
     } else {
         count = 0;
         tocar.pause()
         playBtn.innerHTML = "   &nbsp;&#9654;"
-        playBtn.style.color ="#f63fed"
-        playBtn.style.fontSize= "19pt"
+        playBtn.style.color = "#f63fed"
+        playBtn.style.fontSize = "19pt"
+        playBtn.style.opacity = 1
 
     }
 }
